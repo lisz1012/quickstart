b8a0dcde96ca185a4da795a1b4ef0183440a3f62
lisz1012 <lisz1012@163.com>
2020-05-31 13:54:24 -0700

    Initial commit

---
+++ src/test/java/io/opencensus/metrics/quickstart/ReplTest.java
@@ -0,0 +1,20 @@
+package io.opencensus.metrics.quickstart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ReplTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
